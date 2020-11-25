35090e49c2e5972da8c1928097777b1bfeb639c4
jgower05 <71405084+jgower05@users.noreply.github.com>
2020-11-25 19:24:50 +0000

    Handgun and Gun framework

---
+++ Component 3 Horror Game/Assets/Scripts/Gun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    private string name;  //name of the specific weapon
+    private int magazineCount;  //amount of bullets that can be stored in the gun's magazine. eg a handgun can store a maximum of seven bullets.
+    private float reloadTime, firingTime; //the time taken for the gun to be reloaded and the time taken for the next bullet to be avaliable to fire.
+    private int damage; //The amount of the damage the gun will deal, handguns will deal less damage compared to a gun like a shotgun.
+
+    public string Name { get => name; set => name = value; }
+    public int MagazineCount { get => magazineCount; set => magazineCount = value; }
+    public float ReloadTime { get => reloadTime; set => reloadTime = value; }
+    public float FiringTime { get => firingTime; set => firingTime = value; }
+    public int Damage { get => damage; set => damage = value; }
+
+    public virtual void Fire() { 
+        //Handles the code which will be called as soon as the left input button
+        // is pressed. This will vary depending on the type of gun, for example, the
+        //firing rate should be different
+    }
+}
---
+++ Component 3 Horror Game/Assets/Scripts/Weapons/Handgun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handgun : Gun
+{
+    public int magazineCount = 7;
+    public int ammoCount = 35;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Name = "Handgun";
+        MagazineCount = 7;
+        ReloadTime = 0.25f;
+        FiringTime = 0.15f;
+        Damage = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            //Fire the gun
+            Debug.Log("The gun has fired! Pew pew!");
+        }
+    }
+}
