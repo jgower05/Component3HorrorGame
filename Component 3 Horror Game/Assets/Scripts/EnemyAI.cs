7fe36332bd786c908f057f18aaefcafad50d0951
jgower05 <71405084+jgower05@users.noreply.github.com>
2021-01-02 01:28:17 +0000

    Enemy Attacking System

--- Component 3 Horror Game/Assets/Scripts/EnemyAI.cs
+++ Component 3 Horror Game/Assets/Scripts/EnemyAI.cs
@@ -11,14 +11,21 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private float health;
     [SerializeField] private float enemySpeed;
+    [SerializeField] private float enemySprintingSpeed;
     [SerializeField] private float enemyDamage;
     [SerializeField] private float enemyRotationSpeed;
     [SerializeField] private Transform playerTransform;
     [SerializeField] private GameObject player;
+    [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private Transform enemyTransform;
     [SerializeField] private LineOfSight lineOfSight;
     [SerializeField] private FindPath pathFinder;
     [SerializeField] private WaypointIdentifier waypointIdentifier;
+    [SerializeField] private Stings sting;
+
+    [SerializeField] public float attackCooldownTimer;
+    [SerializeField] public float setAttackCooldownTimer = 1.25f;
+    public bool attackedPlayer = false;
 
     public bool reachedNewDestination;
     private List<AStarNode> newPath;
@@ -34,6 +41,7 @@ public class EnemyAI : MonoBehaviour
     private GameObject investigatePoint;
 
     void Start() {
+        attackCooldownTimer = setAttackCooldownTimer;
         ConstructBehaviourTree();
     }
 
@@ -49,7 +57,12 @@ public class EnemyAI : MonoBehaviour
         ActionNode MoveToWaypointNode = new ActionNode(MoveToWaypoint);
         Sequence wanderSequence = new Sequence(new List<Node> { CheckWaypointsAreAvailableNode, SelectWaypointNode, MoveToWaypointNode});
 
-        rootNode = new Selectors(new List<Node> { chaseSequence, wanderSequence});
+        //Nodes relating to the attack sequence
+        ActionNode IsPlayerInAttackProximityNode = new ActionNode(IsPlayerInAttackProximity);
+        ActionNode DealDamageToPlayerNode = new ActionNode(DealDamageToPlayer);
+        Sequence attackSequence = new Sequence(new List<Node> { IsPlayerInAttackProximityNode, DealDamageToPlayerNode });
+
+        rootNode = new Selectors(new List<Node> { chaseSequence, attackSequence, wanderSequence});
     }
 
     public void FindPoint(Vector3 pointPosition) {
@@ -58,6 +71,13 @@ public class EnemyAI : MonoBehaviour
 
     private void Update() {
         rootNode.Evaluate();
+        if (attackedPlayer) {
+            attackCooldownTimer -= Time.deltaTime;
+            if (attackCooldownTimer <= 0) {
+                attackedPlayer = false;
+                attackCooldownTimer = setAttackCooldownTimer;
+            }
+        }
     }
 
 
@@ -75,10 +95,11 @@ public class EnemyAI : MonoBehaviour
 
     //Move To Player
     private NodeStates MoveToPlayer() {
-        if (Vector3.Distance(transform.position, lineOfSight.visibleTargets[0].position) > 0.75f)
+        positionOfPoint = lineOfSight.visibleTargets[0].position;
+        if (Vector3.Distance(transform.position, positionOfPoint) > 0.75f)
         {
-            newPath = RequestPath(transform.position, lineOfSight.visibleTargets[0].position);
-            MoveAlongPath();
+            newPath = RequestPath(transform.position, positionOfPoint);
+            MoveAlongPath(enemySprintingSpeed);
             return NodeStates.SUCCESS;
         }
         return NodeStates.FAILURE;
@@ -86,11 +107,11 @@ public class EnemyAI : MonoBehaviour
 
     //Move the enemy to a waypoint position
     private NodeStates MoveToWaypoint() {
-        UnityEngine.Debug.Log($"Distance between enemy and waypoint: {Vector3.Distance(transform.position, positionOfPoint)}");
+        //UnityEngine.Debug.Log($"Distance between enemy and waypoint: {Vector3.Distance(transform.position, positionOfPoint)}");
         if (Vector3.Distance(transform.position, positionOfPoint) > 3f)
         {
             newPath = RequestPath(transform.position, positionOfPoint);
-            MoveAlongPath();
+            MoveAlongPath(enemySpeed);
             return NodeStates.RUNNING;
         }
         else {
@@ -133,20 +154,49 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    public void MoveAlongPath()
+    //Check if player is close enough for the enemy to attack.
+    private NodeStates IsPlayerInAttackProximity() {
+        if (sting.isPlayerInProximityOfAttack)
+        {
+            return NodeStates.SUCCESS;
+        }
+        else {
+            return NodeStates.FAILURE;
+        }
+    }
+
+    //Deal damage to player
+    private NodeStates DealDamageToPlayer() {
+        if (!attackedPlayer)
+        {
+            //Deal damage to player
+            playerMovement.currentHealth -= enemyDamage;
+            attackedPlayer = true;
+            return NodeStates.SUCCESS;
+        }
+        else {
+            return NodeStates.FAILURE;
+        }
+    }
+
+    public void MoveAlongPath(float speed)
     {
         foreach (AStarNode pathNodes in newPath)
         {
             Vector3 newPos = new Vector3(pathNodes.worldPosition.x, transform.position.y, pathNodes.worldPosition.z);
-            transform.position = Vector3.MoveTowards(transform.position, newPos, enemySpeed * Time.deltaTime);
-            //SmoothLookAt(newPos);
+            transform.position = Vector3.MoveTowards(transform.position, newPos, speed * Time.deltaTime);
+            SmoothLookAt(positionOfPoint);
             
         }
         reachedNewDestination = true;
     }
 
+    //Rotates the enemy to face the target position on the Y-axis
     public void SmoothLookAt(Vector3 targetPosition) {
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(targetPosition), enemyRotationSpeed * Time.deltaTime);
+        var rotation = Quaternion.LookRotation(targetPosition - transform.position);
+        rotation.x = 0;
+        rotation.z = 0;
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * enemyRotationSpeed);
     }
 
     public List<AStarNode> RequestPath(Vector3 startPos, Vector3 endPos)
---
+++ Component 3 Horror Game/Assets/Stings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stings : MonoBehaviour
+{
+    public bool isPlayerInProximityOfAttack = false;
+
+    //Player can be attacked if they enter the collider of the sting
+    void OnTriggerEnter(Collider collider) {
+        if (collider.CompareTag("Player"))
+        {
+            isPlayerInProximityOfAttack = true;
+        }
+    }
+
+    //Player cannot be attacked if they exit the collider of the sting
+    void OnTriggerExit(Collider collider) {
+        if (collider.CompareTag("Player")) {
+            isPlayerInProximityOfAttack = false;
+        }
+    }
+}
